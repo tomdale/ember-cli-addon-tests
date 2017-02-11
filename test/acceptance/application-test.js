1ee81da424c3817399b95766c66170404551991b
kellyselden <kellyselden@gmail.com>
2017-02-11 09:45:25 -0800

    generate fixture in a temp dir instead of saved in git

--- test/acceptance/application-test.js
+++ test/acceptance/application-test.js
@@ -3,6 +3,9 @@ var expect = require('chai').expect;
 var RSVP = require('rsvp');
 var request = RSVP.denodeify(require('request'));
 var AddonTestApp = require('../../lib').AddonTestApp;
+var runEmber = require('../../lib/utilities/run-ember');
+var makeTemp = require('temp').track();
+var writeFile = RSVP.denodeify(require('fs').writeFile);
 
 describe('Acceptance | application', function() {
   this.timeout(300000);
@@ -12,12 +15,23 @@ describe('Acceptance | application', function() {
 
   before(function() {
     previousCwd = process.cwd();
-    process.chdir('test/fixtures/my-addon');
+    var tmp = makeTemp.mkdirSync();
+    process.chdir(tmp);
 
     app = new AddonTestApp();
 
-    return app.create('dummy', {
-      fixturesPath: 'test/fixtures/my-addon/tests'
+    return runEmber(
+      'addon', ['my-addon', '-sb', '-sn', '-sg']
+    ).then(function() {
+      process.chdir(path.join(tmp, 'my-addon'));
+      return writeFile(
+        'tests/dummy/app/templates/application.hbs',
+        'The dummy app is rendering correctly'
+      );
+    }).then(function() {
+      return app.create('dummy', {
+        fixturesPath: 'tests'
+      });
     }).then(function() {
       return app.startServer();
     });
