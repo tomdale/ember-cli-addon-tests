f8146fdd5e6388281633bf068379e6f95b822252
Trent Willis <trentmwillis@gmail.com>
2017-05-24 21:59:49 -0700

    Genericize precook-node-modules script

--- scripts/precook-node-modules.js
+++ scripts/precook-node-modules.js
@@ -3,6 +3,8 @@
 var temp             = require('temp').track();
 var path             = require('path');
 var fs               = require('fs-promise');
+var readJsonSync     = require('fs-extra').readJsonSync;
+var findup           = require('findup-sync');
 
 var moveDirectory    = require('../lib/utilities/move-directory');
 var runNew           = require('../lib/utilities/run-new');
@@ -10,17 +12,20 @@ var symlinkDirectory = require('../lib/utilities/symlink-directory');
 
 var tmpDir           = temp.mkdirSync();
 var root             = process.cwd();
-var name             = 'precooked-app';
+var appName          = 'precooked-app';
+
+var pkg = findup('package.json');
+var name = readJsonSync(pkg).name;
 
 fs.ensureDir('tmp')
   .then(function() {
     process.chdir(tmpDir);
-    return runNew(name);
+    return runNew(appName);
   })
   .then(function() {
     var precooked = path.join(root, 'tmp', 'precooked_node_modules');
-    moveDirectory(path.join(tmpDir, name, 'node_modules'), precooked);
-    symlinkDirectory(root, path.join(precooked, 'ember-cli-fastboot'));
+    moveDirectory(path.join(tmpDir, appName, 'node_modules'), precooked);
+    symlinkDirectory(root, path.join(precooked, name));
   })
   .catch(function(e) {
     console.log(e); // eslint-disable-line no-console
