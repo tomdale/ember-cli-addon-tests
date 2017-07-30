6e965893d191d98cb75d8569b497603e437435c9
Kelly Selden <kellyselden@gmail.com>
2017-07-29 21:16:48 -0700

    make a simple test, so the complicated test can grow

---
+++ test/acceptance/simple.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const expect = require('chai').expect;
+const RSVP = require('rsvp');
+const request = RSVP.denodeify(require('request'));
+const AddonTestApp = require('../../lib').AddonTestApp;
+const createAddon = require('../helpers/create-addon');
+
+describe('Acceptance | simple', function() {
+  this.timeout(process.platform === 'win32' ? 500000 : 300000);
+
+  let app;
+
+  before(function() {
+    app = new AddonTestApp();
+
+    return createAddon(() => {
+      return app.create('dummy', {
+        fixturesPath: 'tests'
+      });
+    });
+  });
+
+  beforeEach(function() {
+    return app.startServer();
+  });
+
+  afterEach(function() {
+    return app.stopServer();
+  });
+
+  it('works', function() {
+    return request('http://localhost:49741/assets/vendor.js')
+      .then(function(response) {
+        expect(response.body).to.contain('my-addon is working');
+      });
+  });
+});
