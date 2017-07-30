2dc76cc27aa86caae1ec5bb42e412dacc24fe5f3
Kelly Selden <kellyselden@gmail.com>
2017-07-29 20:05:12 -0700

    add simple eslint rules

--- scripts/precook-node-modules.js
+++ scripts/precook-node-modules.js
@@ -1,21 +1,21 @@
-"use strict";
+'use strict';
 
-var temp             = require('temp').track();
-var path             = require('path');
-var fs               = require('fs-promise');
-var readJsonSync     = require('fs-extra').readJsonSync;
-var findup           = require('findup-sync');
+const temp             = require('temp').track();
+const path             = require('path');
+const fs               = require('fs-promise');
+const readJsonSync     = require('fs-extra').readJsonSync;
+const findup           = require('findup-sync');
 
-var moveDirectory    = require('../lib/utilities/move-directory');
-var runNew           = require('../lib/utilities/run-new');
-var symlinkDirectory = require('../lib/utilities/symlink-directory');
+const moveDirectory    = require('../lib/utilities/move-directory');
+const runNew           = require('../lib/utilities/run-new');
+const symlinkDirectory = require('../lib/utilities/symlink-directory');
 
-var tmpDir           = temp.mkdirSync();
-var root             = process.cwd();
-var appName          = 'precooked-app';
+const tmpDir           = temp.mkdirSync();
+const root             = process.cwd();
+const appName          = 'precooked-app';
 
-var pkg = findup('package.json');
-var name = readJsonSync(pkg).name;
+const pkg = findup('package.json');
+const name = readJsonSync(pkg).name;
 
 fs.ensureDir('tmp')
   .then(function() {
@@ -23,7 +23,7 @@ fs.ensureDir('tmp')
     return runNew(appName);
   })
   .then(function() {
-    var precooked = path.join(root, 'tmp', 'precooked_node_modules');
+    let precooked = path.join(root, 'tmp', 'precooked_node_modules');
     moveDirectory(path.join(tmpDir, appName, 'node_modules'), precooked);
     symlinkDirectory(root, path.join(precooked, name));
   })
